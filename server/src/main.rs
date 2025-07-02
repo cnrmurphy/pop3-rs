59c063b185c31bce8d61146b8e8d9d5a233f0171
cnrmurphy <conor.murphy4304@gmail.com>
2025-07-02 19:23:34 -0400

    add basic session manager with resource locking

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,10 @@
 pub mod protocol;
 
+use std::{
+    collections::HashSet,
+    sync::{Arc, Mutex},
+};
+
 use protocol::{Command, SessionState, StatusIndicator};
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter},
@@ -10,26 +15,73 @@ pub type IOResult<T> = std::io::Result<T>;
 
 const PORT: u16 = 110;
 
-#[derive(Debug)]
 pub struct Session {
     state: SessionState,
     user: Option<String>,
+    mailbox_lock: Option<MailboxLock>,
+}
+
+pub struct SessionManager {
+    locked_mailboxes: Mutex<HashSet<String>>,
+}
+
+impl SessionManager {
+    pub fn new() -> Self {
+        Self {
+            locked_mailboxes: Mutex::new(HashSet::new()),
+        }
+    }
+
+    pub fn try_lock_mailbox(
+        &self,
+        username: &str,
+        manager_arc: Arc<SessionManager>,
+    ) -> Result<MailboxLock, &'static str> {
+        let mut lock = self.locked_mailboxes.lock().unwrap();
+        if lock.contains(username) {
+            Err("Mailbox already locked")
+        } else {
+            lock.insert(username.to_string());
+            Ok(MailboxLock {
+                username: username.to_string(),
+                manager: manager_arc,
+            })
+        }
+    }
+
+    fn unlock_mailbox(&self, username: &str) {
+        let mut lock = self.locked_mailboxes.lock().unwrap();
+        lock.remove(username);
+    }
+}
+
+pub struct MailboxLock {
+    username: String,
+    manager: Arc<SessionManager>,
+}
+
+impl Drop for MailboxLock {
+    fn drop(&mut self) {
+        self.manager.unlock_mailbox(&self.username);
+    }
 }
 
 #[tokio::main]
 async fn main() {
+    let session_manager = Arc::new(SessionManager::new());
     let listener = TcpListener::bind("127.0.0.1:1110").await.unwrap();
 
     loop {
         let (stream, addr) = listener.accept().await.unwrap();
+        let session_manager = Arc::clone(&session_manager);
         println!("new connection");
         tokio::spawn(async move {
-            process(stream).await;
+            process(stream, session_manager).await;
         });
     }
 }
 
-async fn process(mut stream: TcpStream) -> IOResult<()> {
+async fn process(mut stream: TcpStream, session_manager: Arc<SessionManager>) -> IOResult<()> {
     let (reader, writer) = stream.split();
     let mut reader = BufReader::new(reader);
     let mut writer = BufWriter::new(writer);
@@ -41,17 +93,17 @@ async fn process(mut stream: TcpStream) -> IOResult<()> {
     let mut session = Session {
         state: SessionState::Authorization,
         user: None,
+        mailbox_lock: None,
     };
     let mut line = String::new();
 
     loop {
-        println!("{:?}", session);
         line.clear();
         reader.read_line(&mut line).await?;
         match Command::parse(line.trim()) {
             Ok(cmd) => {
                 let should_quit = matches!(cmd, Command::Quit);
-                let resp = handle_command(cmd, &mut session);
+                let resp = handle_command(cmd, &mut session, &session_manager);
                 send_response(&mut writer, resp).await?;
                 if should_quit {
                     return Ok(());
@@ -75,7 +127,11 @@ async fn send_response(
     Ok(())
 }
 
-fn handle_command(cmd: Command, session: &mut Session) -> StatusIndicator {
+fn handle_command(
+    cmd: Command,
+    session: &mut Session,
+    session_manager: &Arc<SessionManager>,
+) -> StatusIndicator {
     match cmd {
         Command::Apop => StatusIndicator::Ok("APOP".to_string()),
         Command::User(username) => {
@@ -92,8 +148,15 @@ fn handle_command(cmd: Command, session: &mut Session) -> StatusIndicator {
             if session.user.is_none() {
                 return StatusIndicator::Err("No username set - send USER first".to_string());
             }
-            session.state = SessionState::Transaction;
-            StatusIndicator::Ok("Password accepted".to_string())
+            let username = session.user.as_ref().unwrap();
+            match session_manager.try_lock_mailbox(username, Arc::clone(session_manager)) {
+                Ok(lock) => {
+                    session.state = SessionState::Transaction;
+                    session.mailbox_lock = Some(lock);
+                    StatusIndicator::Ok("Pass accepted".to_string())
+                }
+                Err(_) => StatusIndicator::Err("Mailbox already in use".to_string()),
+            }
         }
         Command::Noop => StatusIndicator::Ok("NOOP".to_string()),
         Command::Quit => {
